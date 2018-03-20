e96e6020a426753b74fbcda81b0118fba56793e9
Akshay yadav <akshaysips@gmail.com>
2018-03-21 00:50:37 +0530

    First Commit

---
+++ app/src/main/java/com/example/akshay/farmconnect/SoilAppointmentFragment.java
@@ -0,0 +1,29 @@
+package com.example.akshay.farmconnect;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SoilAppointmentFragment extends Fragment {
+
+
+    public SoilAppointmentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_soil_appointment, container, false);
+    }
+
+}
