19e65a4a8c1663ff571e3e8c69527c96f72b7fab
Akshay yadav <akshaysips@gmail.com>
2018-03-23 00:28:47 +0530

    soil appointment updated

--- app/src/main/java/com/example/akshay/farmconnect/HomeFragment.java
+++ app/src/main/java/com/example/akshay/farmconnect/HomeFragment.java
@@ -1,29 +1,30 @@
 package com.example.akshay.farmconnect;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class HomeFragment extends Fragment {
 
 
     public HomeFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//abhay test
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        Button loginButton;
+      //  loginButton = (Button) view.findViewById(R.id.login);
+        return view;
+    }
 }
+
--- app/src/main/java/com/example/akshay/farmconnect/MainActivity.java
+++ app/src/main/java/com/example/akshay/farmconnect/MainActivity.java
@@ -25,6 +25,10 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.flMain,new HomeFragment());
+        ft.commit();
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
