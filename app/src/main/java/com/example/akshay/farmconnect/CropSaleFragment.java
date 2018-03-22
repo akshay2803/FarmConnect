277aa113a0af6ae3bed0e0a14880bba7c687f9e8
abhay40 <abhaygargmzn@gmail.com>
2018-03-22 21:28:15 +0530

    abhay test

--- app/src/main/java/com/example/akshay/farmconnect/CropSaleFragment.java
+++ app/src/main/java/com/example/akshay/farmconnect/CropSaleFragment.java
@@ -6,24 +6,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CropSaleFragment extends Fragment {
+    ListView lv;
 
-
+    String[]cropArray = {"Rice","Wheat","Barley","Maize",
+            "Oats"};
+    String[]desc={"50kg","40kg","30kg","60kg","90kg"};
+    Integer[]imgid={R.drawable.rice,R.drawable.wheat,R.drawable.barley,R.drawable.maize,R.drawable.oats};
     public CropSaleFragment() {
-        // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crop_sale, container, false);
+        View v= inflater.inflate(R.layout.fragment_crop_sale, container, false);
+        lv=(ListView) v.findViewById(R.id.listCrop);
+        CustomListview customListview=new CustomListview(this,cropArray,desc,imgid);
+
     }
+
+
 
 }
